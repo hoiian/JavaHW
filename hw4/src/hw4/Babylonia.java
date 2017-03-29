35d4a5c621e1078f3cfac78b538e17d737582c39
hoiian <hoiian96@gmail.com>
2017-03-30 01:03:24 +0800

    ~~

--- hw4/src/hw4/Babylonia.java
+++ hw4/src/hw4/Babylonia.java
@@ -1,13 +1,17 @@
 package hw4;
 import java.util.Scanner;
-import java.lang.*;
+import java.text.DecimalFormat;
 
 public class Babylonia {
+	private static Scanner scan;
+
 	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
+		scan = new Scanner(System.in);
 		long n = 0;
-		double guess=0.0, lastguess=0.0,a;
-		System.out.print("number = ");
+		double guess=0.0, lastguess=0.0;
+		DecimalFormat pattern = new DecimalFormat("#.##");
+		
+//		System.out.print("number = ");
 		n = scan.nextLong();
 		
 		guess = n/2;
@@ -16,11 +20,11 @@ public class Babylonia {
 			guess = (guess + (n/guess) ) / 2;
 		}while( (guess<lastguess*0.99) || (guess>lastguess*1.01) );
 
-		System.out.println(guess);		
-		System.out.printf("%.1f",guess);
-		System.out.println();
-		a = Math.sqrt(n);
-		System.out.printf("%.1f",a);
+//		System.out.println(guess);
+		System.out.println(pattern.format(guess));
+//		System.out.println();
+//		a = Math.sqrt(n);
+//		System.out.printf("%.1f",a);
 	}
 
 }
